4926b1558c85ac5793e9bfa0ba19745e29ffbf44
RachelTWQ <racheltang15@hotmail.com>
2019-05-14 03:42:22 +0000

    remove unnecessary instance variable

--- question.rb
+++ question.rb
@@ -9,8 +9,8 @@ class Question
     def ask_question(current_player)
         puts "#{current_player.name}: What does #{@num1} plus #{@num2} equal?"
         print ">"
-        @answer = $stdin.gets.chomp.to_i
-        if @result != @answer
+        answer = $stdin.gets.chomp.to_i
+        if @result != answer
             puts "#{current_player.name}: Seriously? No!"
             current_player.reduce
         else
